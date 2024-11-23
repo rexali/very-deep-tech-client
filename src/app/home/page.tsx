c23075a810d1f8d52c0e31fa9f236e503d40f238
Aliyu <aliyubababello@gmail.com>
2024-11-23 16:43:05 +0100

    homepage updated

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,11 +8,15 @@ import Copyright from '@/components/common/copyright';
 import SearchInput from "../search/SearchInput";
 import FeaturedProducts from "./FeaturedProducts";
 import CreateSubscription from "../subscriptions/CreateSubscription";
-import Partners from "./Partners";
-import Portfolios from "./Portfolios";
-import Team from "./Team";
-import Testimonials from "./Testimonials";
+// import Partners from "./Partners";
+// import Portfolios from "./Portfolios";
+// import Team from "./Team";
+// import Testimonials from "./Testimonials";
 import { useMediaQuery } from "react-responsive";
+import Testimonials from './Testimonials';
+import Portfolios from './Portfolios';
+import Team from './Team';
+import Partners from './Partners';
 
 export default function HomePage() {
   // const isMobile = useMediaQuery({ maxDeviceWidth: 1023 });
