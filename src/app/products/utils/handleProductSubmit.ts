ca269cf3d1d813919259f817b5b57eb1d533f496
Aliyu <aliyubababello@gmail.com>
2024-11-27 18:01:17 +0100

    ProductDetails changed

--- src/app/products/utils/handleProductSubmit.ts
+++ src/app/products/utils/handleProductSubmit.ts
@@ -5,7 +5,6 @@ const handleProductSubmit = async (event: any, setPostSuccess: any, setPostError
     event.preventDefault();
     const {
         product_name,
-        product_pictures,
         product_category,
         product_sub_category,
         product_description,
@@ -19,22 +18,22 @@ const handleProductSubmit = async (event: any, setPostSuccess: any, setPostError
     } = event.target.elements;
 
     let formData = new Form();
-    formData.append('product_name', product_name.value);
-    let filesObject: any;
+
     try {
-        filesObject = document.querySelector('#product_pictures') as any;
+        let filesObject = document.querySelector('#product_pictures') as any;
         for (let file of filesObject.files) {
             formData.append('product_pictures[]', file);
         }
     } catch (error) {
         console.log(error)
     }
+    formData.append('product_name', product_name.value);
     formData.append('product_category', product_category.value);
     formData.append('product_sub_category', product_sub_category.value);
     formData.append('product_description', product_description.value,);
     formData.append('product_price', product_price.value);
     formData.append('product_quantity', product_quantity.value);
-    formData.append('product_weight', product_weight?.value);
+    formData.append('product_weight', product_weight.value);
     formData.append('product_size', product_size.value);
     formData.append('product_code', product_code.value);
     formData.append('product_demos_links', product_demos_links.value);
