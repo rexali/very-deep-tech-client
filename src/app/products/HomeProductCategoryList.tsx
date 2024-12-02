402a7a913e7108022c9092a33b42def2c1e86ff7
Aliyu <aliyubababello@gmail.com>
2024-12-02 11:22:41 +0100

    unique categories

--- src/app/products/HomeProductCategoryList.tsx
+++ src/app/products/HomeProductCategoryList.tsx
@@ -10,19 +10,20 @@ import Button from '@mui/material/Button';
 
 export default function HomeProductCategoryList(props: any) {
     let products = props.products ?? [];
+    let categories = Array.from(new Set(products.map((category: any) => category.product_category.toUpperCase())));
 
     return (
         <div>
             <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} marginTop={5} display={"flex"} justifyContent={'center'}>
                 {
-                    products.map((product: any, i: number) => {
+                    categories.map((category: any, i: number) => {
                         return (
-                            <Grid item key={product._id + 'category'} xs={6} sm={6} md={6} lg={4}>
+                            <Grid item key={category} xs={6} sm={6} md={6} lg={4}>
                                 <Card sx={{ backgroundColor: 'green', maxWidth: '100%', MaxHeight: '100%', textAlign: 'center', alignSelf: 'center' }}>
                                     <CardContent>
-                                        <Link prefetch style={{ textDecoration: 'none', }} href={`/category/?term=${product.product_category}`}>
+                                        <Link prefetch style={{ textDecoration: 'none', }} href={`/category/?term=${category.toLowerCase()}`}>
                                             <Button sx={{ fontSize: 11, color: 'white' }}>
-                                                {product.product_category.toUpperCase()}
+                                                {category}
                                             </Button>
                                         </Link>
                                     </CardContent>
