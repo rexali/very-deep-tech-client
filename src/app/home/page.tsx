1e33c6c24aabf83ae3d21938b6406d426fe5b3e2
Aliyu <aliyubababello@gmail.com>
2024-12-06 23:58:42 +0100

    home corrected3

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,86 +1,10 @@
-// 'use client'
+import {Container} from "@mui/material";
 
-// import React from 'react';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import MarketingMessage from './MarketingMessage';
-// import FAQHowItWorks from './FAQHowItWorks';
-// import Copyright from '@/components/common/copyright';
-// import SearchInput from "../search/SearchInput";
-// import CreateSubscription from "../subscriptions/CreateSubscription";
-// import { useMediaQuery } from "react-responsive";
-// import Testimonials from './testimonials';
-// import Portfolios from './portfolios';
-// import Team from './team';
-// import Partners from './partners';
-// import HomeProductCategories from '../products/HomeProductCategories';
-// import FeaturedProducts from '../products/FeaturedProducts';
-// import PopularProducts from '../products/PopularProducts';
-// import Link from 'next/link';
-// import Box from '@mui/material/Box';
-// import RecommendedProducts from '../products/RecommendedProducts';
-// import NewProducts from '../products/NewProducts';
+export default function Page() {
 
-// export default function HomePage(props: any) {
-//   // const isMobile = useMediaQuery({ maxDeviceWidth: 1023 });
-//   let data = props.data ?? []
-
-//   return (
-//     <main>
-//       {/* <CssBaseline /> */}
-//       <SearchInput />
-//       <MarketingMessage /> <br /><br />
-//       <HomeProductCategories categories={data} />
-//       <FeaturedProducts products={data} />
-//       <Box margin={2} padding={2} display={"flex"} justifyContent={'center'}>
-//         <Link
-//           style={{ textDecoration: "none", color: 'green' }}
-//           type="button"
-//           color="success"
-//           href={`/products`}
-//         >
-//           View all
-//         </Link>
-//       </Box>
-//       <PopularProducts products={data} />
-//       <Box marginTop={2} padding={2} display={"flex"} justifyContent={'center'}>
-//         <Link
-//           style={{ textDecoration: "none", color: 'green' }}
-//           type="button"
-//           color="success"
-//           href={`/products`}
-//         >
-//           View all
-//         </Link>
-//       </Box>
-//       <RecommendedProducts />
-//       <Box marginTop={2} padding={2} display={"flex"} justifyContent={'center'}>
-//         <Link
-//           style={{ textDecoration: "none", color: 'green' }}
-//           type="button"
-//           color="success"
-//           href={`/products`}
-//         >
-//           View all
-//         </Link>
-//       </Box>
-//       <NewProducts products={data} />
-//       <Box marginTop={2} padding={2} display={"flex"} justifyContent={'center'}>
-//         <Link
-//           style={{ textDecoration: "none", color: 'green' }}
-//           type="button"
-//           color="success"
-//           href={`/products`}
-//         >
-//           View all
-//         </Link>
-//       </Box>
-//       <Testimonials />
-//       <Portfolios />
-//       <Team />
-//       <FAQHowItWorks />
-//       <Partners />
-//       <CreateSubscription />
-//       <Copyright sx={{ mt: 4, mb: 4 }} />
-//     </main>
-//   );
-// }
+  return (
+    <Container maxWidth="md" component={'main'} sx={{mt:10}}>
+      <p>About</p>
+    </Container>
+  )
+}
