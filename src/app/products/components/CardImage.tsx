30c85848266b8c31cc2cfceb5f02ef17be14da44
Aliyu <aliyubababello@gmail.com>
2024-12-12 06:46:02 +0100

    video and image updated4

--- src/app/products/components/CardImage.tsx
+++ src/app/products/components/CardImage.tsx
@@ -3,34 +3,25 @@ import Image from 'next/image';
 import placeholderImage from '@/assets/images/cshop.png'
 import * as React from "react";
 
-export default function CardImage(props:any) {
+export default function CardImage(props: any) {
     const [imgSrc, setImgSrc] = React.useState<any>(props.src);
-    // const dataUrl = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAVQAAABkCAYAAADZn8isAAABFElEQVR42u3UMQEAAAQAMHL4FFVYCCG8W4hlb00A8JZCBRAqgFABhAqAUAGECiBUAKECIFQAoQIIFQChAggVQKgAQgVAqABCBRAqgFABECqAUAGECoBQAYQKIFQAoQIgVAChAggVQKgACBVAqABCBUCoAEIFECqAUAEQKoBQAYQKgFABhAogVAChAiBUAKECCBVAqAAIFUCoAEIFQKgAQgUQKoBQARAqgFABhAogVACECiBUAKECIFQAoQIIFUCoAAgVQKgAQgUQqlABhAogVAChAiBUAKECCBVAqAAIFUCoAEIFQKgAQgUQKoBQARAqgFABhAogVACECiBUAKECIFQAoQIIFUCoAAgVQKgAQgUQKgA/Bz0MpgVVm/VcAAAAAElFTkSuQmCC"
 
     return <Image
-        // src={imgSrc}
-        // alt={props.alt}
-        // {...props}
-        // placeholder="blur"
-        // blurDataURL={dataUrl}
-        // onError={() => {
-        //     setImgSrc(placeholderImage)
-        // }}
         src={imgSrc}
         alt={props.alt}
         style={{
-          display: 'block',
-          marginRight: 'auto',
-          marginLeft: 'auto',
-          borderRadius: 20,
+            display: 'block',
+            marginRight: 'auto',
+            marginLeft: 'auto',
+            borderRadius: 20,
         }}
         width={345}
         height={245}
         placeholder="blur"
         blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAVQAAABkCAYAAADZn8isAAABFElEQVR42u3UMQEAAAQAMHL4FFVYCCG8W4hlb00A8JZCBRAqgFABhAqAUAGECiBUAKECIFQAoQIIFQChAggVQKgAQgVAqABCBRAqgFABECqAUAGECoBQAYQKIFQAoQIgVAChAggVQKgACBVAqABCBUCoAEIFECqAUAEQKoBQAYQKgFABhAogVAChAiBUAKECCBVAqAAIFUCoAEIFQKgAQgUQKoBQARAqgFABhAogVACECiBUAKECIFQAoQIIFUCoAAgVQKgAQgUQqlABhAogVAChAiBUAKECCBVAqAAIFUCoAEIFQKgAQgUQKoBQARAqgFABhAogVACECiBUAKECIFQAoQIIFUCoAAgVQKgAQgUQKgA/Bz0MpgVVm/VcAAAAAElFTkSuQmCC"
         onError={() => {
-          setImgSrc(placeholderImage)
+            setImgSrc(placeholderImage)
         }}
-        
+
     />
 }
--- src/app/products/components/ProductDetailsVideo.tsx
+++ src/app/products/components/ProductDetailsVideo.tsx
@@ -1,19 +1,6 @@
-// 'use client'
 import Fallback from "@/components/common/fallback";
-// import { useEffect, useState } from "react";
+
 export function ProductDetailsVideo({ src }: { src: string }) {
-    // const [videoSrc, setVideoSrc] = useState<any>('');
-    // useEffect(() => {
-    //     async function getVideoSrc() {
-    //         let response = await fetch('https://www.youtube.com/embed/tgbNymZ7vqY');
-    //         if (response.ok) {
-    //             setVideoSrc(await response.json());
-    //         } else {
-    //             setVideoSrc('https://www.youtube.com/embed/tgbNymZ7vqY');
-    //         }
-    //     }
-    //     getVideoSrc();
-    // }, [src]);
 
     if (!src) {
         return <Fallback item={'No Video Demo Now'} />
