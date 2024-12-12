131d6c94d10aa45aa1d5d794a4b07c92592d8f20
Aliyu <aliyubababello@gmail.com>
2024-12-12 06:39:18 +0100

    cartCard updated4

--- src/app/carts/page.tsx
+++ src/app/carts/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Container, Box } from "@mui/material";
-import { useState, useContext, useCallback } from "react";
+import { useState, useContext, useCallback, useEffect } from "react";
 import Fallback from "@/components/common/fallback";
 import { CartListComponent } from "./CartListComponent";
 import { AppContext } from "@/context/AppContext";
@@ -17,8 +17,8 @@ export default function CartPage() {
   const { dispatch } = useContext(AppContext);
   const { user } = useAuth();
   const userId = user?._id || getToken('_id') as string;
-  const { carts } = useUserCarts(userId, dispatch, activePage);
-  const [products, setProducts] = useState<Array<any>>(carts);
+  // const { carts } = useUserCarts(userId, dispatch, activePage);
+  const [products, setProducts] = useState<Array<any>>([]);
 
   const getCartData = useCallback(async () => {
     let productsInCart = await getUserCartsAPI(userId, activePage);
@@ -26,18 +26,23 @@ export default function CartPage() {
     dispatch(getCarts(productsInCart));
   }, [activePage, dispatch, userId])
 
+  useEffect(() => {
+    getCartData();
+  }, [getCartData])
+
+
   if (!products?.length) {
     return <Fallback item={"No product in your cart yet"} />
   }
 
   return (
     <Container maxWidth="lg" component={'main'} sx={{ mt: 10 }} >
-      <Box>Carts: {carts[0]?.totalCarts}</Box>
+      <Box>Carts: {products[0]?.totalCarts}</Box>
       <CartListComponent
         products={products}
         activePage={activePage}
         setActivePage={setActivePage}
-        totalCarts={carts[0]?.totalCarts}
+        totalCarts={products[0]?.totalCarts}
         refreshCart={getCartData}
       />
     </Container>
