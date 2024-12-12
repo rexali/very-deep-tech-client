7619c86530b7c63bcc3d7435874e25b9210e44e3
Aliyu <aliyubababello@gmail.com>
2024-12-12 06:30:39 +0100

    video and image updated3

--- src/app/products/components/ProductDetailsVideo.tsx
+++ src/app/products/components/ProductDetailsVideo.tsx
@@ -1,24 +1,23 @@
-'use client'
+// 'use client'
 import Fallback from "@/components/common/fallback";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 export function ProductDetailsVideo({ src }: { src: string }) {
-    const [videoSrc, setVideoSrc] = useState<any>('');
+    // const [videoSrc, setVideoSrc] = useState<any>('');
+    // useEffect(() => {
+    //     async function getVideoSrc() {
+    //         let response = await fetch('https://www.youtube.com/embed/tgbNymZ7vqY');
+    //         if (response.ok) {
+    //             setVideoSrc(await response.json());
+    //         } else {
+    //             setVideoSrc('https://www.youtube.com/embed/tgbNymZ7vqY');
+    //         }
+    //     }
+    //     getVideoSrc();
+    // }, [src]);
 
-    useEffect(() => {
-        async function getVideoSrc() {
-            let response = await fetch('https://www.youtube.com/embed/tgbNymZ7vqY');
-            if (response.ok) {
-                setVideoSrc(await response.json());
-            } else {
-                setVideoSrc('https://www.youtube.com/embed/tgbNymZ7vqY');
-            }
-        }
-        getVideoSrc();
-    }, [src]);
-
-    if (!videoSrc) {
+    if (!src) {
         return <Fallback item={'No Video Demo Now'} />
     }
 
-    return <iframe src={videoSrc} width={420} height={315} />
+    return <iframe src={src} width={420} height={315} />
 }
