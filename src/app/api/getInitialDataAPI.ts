bc969d7cc9708695368eb768df38b198873ab81a
Aliyu Bello <aliyubababello@gmail.com>
2025-01-12 21:12:04 +0100

    update getProductsAPI

--- src/app/api/getInitialDataAPI.ts
+++ src/app/api/getInitialDataAPI.ts
@@ -1,6 +1,6 @@
 import { SERVER_URL } from "@/constants/url";
 
-const getInitialDataAPI = async (subdomain?:string, page: number = 1) => {
+const getInitialDataAPI = async (subdomain: string = 'maindomain', page: number = 1) => {
 
     try {
         let data = await fetch(`${SERVER_URL}/initial?page=${page}&subdomain=${subdomain}`, { next: { revalidate: 120 } }).then(res => res.json());
