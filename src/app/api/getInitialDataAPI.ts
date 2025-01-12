161cc8023ec3ea9540a674c53b8a3b12cb51e268
Aliyu Bello <aliyubababello@gmail.com>
2025-01-12 10:52:19 +0100

    update initial api

--- src/app/api/getInitialDataAPI.ts
+++ src/app/api/getInitialDataAPI.ts
@@ -3,7 +3,7 @@ import { SERVER_URL } from "@/constants/url";
 const getInitialDataAPI = async (subdomain?:string, page: number = 1) => {
 
     try {
-        let data = await fetch(`${SERVER_URL}/products/${page}/initial/subdomains/${subdomain}`, { next: { revalidate: 60 } }).then(res => res.json());
+        let data = await fetch(`${SERVER_URL}/products/${page}/initial/subdomains/${subdomain}`, { next: { revalidate: 120 } }).then(res => res.json());
 
         return data.data;
     } catch (error) {
